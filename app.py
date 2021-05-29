5bc44d6d2e2accdf0f985bc4b4c865d1fd684870
CaitlinBevan <caitlin.bevan@my.jcu.edu.au>
2021-05-29 21:36:20 +1000

    Add function to convert between Fahrenheit and Celsius

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+"""
+Prac 10 - Flask Project with Modifications
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -13,6 +17,13 @@ def hello_world():
 @app.route('/greet/<name>')
 def greet(name=""):
     return "Hello {}".format(name)
+
+
+@app.route('/f')
+@app.route('/f/<celsius>')
+def f(celsius=0):
+    fahrenheit = float(celsius) * 9.0 / 5 + 32
+    return f"You inputted {celsius} Celsius which is {fahrenheit:.2f} Fahrenheit."
 
 
 if __name__ == '__main__':
